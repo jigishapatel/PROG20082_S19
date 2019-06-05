ddff4dc3157579ad2f65de77a7c67ec48b9754b4
jkp <jigishapatel28@gmail.com>
2019-06-05 16:51:09 -0400

    Multilinugual support for French(Canada) added.

--- RFMCafe/app/src/main/java/com/jk/rfmcafe/MainActivity.java
+++ RFMCafe/app/src/main/java/com/jk/rfmcafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jk.rfmcafe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -32,6 +33,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     EditText edtName;
 
+    String output = "";
+
+    Double amount = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //refer to UI Views
         this.referWidgets();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        this.resetWidgets();
     }
 
     private void referWidgets(){
@@ -77,6 +87,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_submit:
                 Log.d("MainActivity", "Submit Clicked");
                 this.getValues();
+                this.openNextActivity();
                 break;
         }
     }
@@ -88,13 +99,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rdoSelected = findViewById(rdoCoffee.getCheckedRadioButtonId());
         String coffee = rdoSelected.getText().toString();
 
+        this.calculateAmount();
+
         String addOn = "";
         if (chkWhipped.isChecked()){
             addOn += " Whipped Cream";
+            amount += 0.50;
         }
 
         if (chkEspresso.isChecked()){
             addOn += " Espresso Shot";
+            amount += 0.70;
         }
 
         if (chkSleeve.isChecked()){
@@ -108,9 +123,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String cream = spnCream.getSelectedItem().toString() + " cream ";
         String sugar = spnSugar.getSelectedItem().toString() + " sugar ";
 
-        String output = "Hey " + name + " ! Thank you for your order of " +
+        output = "Hey " + name + " ! Thank you for your order of " +
                 coffee + " " + cream + sugar + " with" + addOn;
         Log.d("MainActivity", output);
         Toast.makeText(getApplicationContext(), output, Toast.LENGTH_LONG).show();
     }
+
+    private void calculateAmount(){
+        switch (rdoSelected.getId()){
+            case R.id.rdo_dark:
+                amount += 1.70;
+                break;
+            case R.id.rdo_original:
+                amount += 1.50;
+                break;
+            case R.id.rdo_french:
+                amount += 2.50;
+                break;
+            case R.id.rdo_iced:
+                amount += 3.00;
+                break;
+        }
+    }
+
+    private void openNextActivity(){
+        Intent receiptIntent = new Intent(this, ReceiptActivity.class);
+        //sending data to next activity
+        receiptIntent.putExtra("EXTRA_OUTPUT", output);
+        receiptIntent.putExtra("EXTRA_AMOUNT", amount);
+        startActivity(receiptIntent);
+    }
+
+    private void resetWidgets(){
+        rdoSelected.setChecked(false);
+        chkWhipped.setChecked(false);
+        chkEspresso.setChecked(false);
+        chkSleeve.setChecked(false);
+        chkDom.setChecked(false);
+        spnCream.setSelection(0);
+        spnSugar.setSelection(0);
+        edtName.setText("");
+        amount = 0.0;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
